ba3a445a79c11ea134cc6b01e70d48246d192bdf
Hunter Padgett <gizmokabanjo@gmail.com>
2022-12-28 13:06:45 -0500

    qr code js bug fix

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ document.getElementById("wifi-form").addEventListener("submit", (e) => {
  e.preventDefault();
 
  // Get the WiFi details from the form
- const ssid = document.getElementById("ssid").value;
- const password = document.getElementById("password").value;
+ const ssid = document.getElementById("ssid").value.trim();
+ const password = document.getElementById("password").value.trim();
 
  // Set the parameters for the WiFi QR code
  const wifi = {
@@ -13,14 +13,14 @@ document.getElementById("wifi-form").addEventListener("submit", (e) => {
   password: password,
  };
 
- console.log(wifi.ssid);
+ console.log(wifi.password);
 
  //  reset the qr code container so that anytime a new qr code is generated, the old one disappears and only a single qr code is displayed
  document.getElementById("qr-code-container").innerHTML = "";
 
  // Create a new QR code object
  const qr = new QRCode(document.getElementById("qr-code-container"), {
-  text: `WIFI:S:${wifi.ssid};T:<WPA|WEP|>;P:${wifi.password};;`,
+  text: `WIFI:T:WPA;S:${wifi.ssid};P:${wifi.password};;`,
  });
 
  // Clear the form text after qr code is created
